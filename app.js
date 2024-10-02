e3431d36cf487a20a56055efe0b508964b1a3496
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-10-02 09:45:54 +0500

    Array

--- app.js
+++ app.js
@@ -69,3 +69,76 @@
 // arr[minIndex] = max;
 // arr[maxIndex] = min;
 // console.log(arr);
+
+// Array misolari
+// 2 Qisim
+
+// 1 misol
+// let n = 5;
+// let result = [];
+// for (let i = 1; i <= n; i++) {
+//     result.push(2 ** i);
+// }
+// console.log(result);
+
+// 2 misol
+// let n = 5;
+// let a = 2;
+// let b = 3;
+// let result = [];
+// result[0] = a;
+// result[1] = b;
+// for (let i = 0; i < n; i++) {
+//     let count = 0;
+//     for (let k = 0; k < result.length; k++) {
+//         count += result[k];
+//     }
+//     result.push(count);
+// }
+// console.log(result);
+
+// 3 misol
+// let n = [1, 2, 3, 4, 5];
+// let result = [];
+// for (let i = 0; i < n.length; i++) {
+//     result.unshift(n[i]);
+// }
+// console.log(result);
+
+// 4 misol
+// let arr = [4, 5, 7, 8, 6, 9];
+// let result = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//         result.push(arr[i]);
+//     }
+// }
+// console.log(result);
+
+// 5 misol
+// let arr = [4, 5, 7, 8, 6, 9];
+// let result1 = [];
+// let result2 = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//         result2.push(arr[i]);
+//     } else {
+//         result1.push(arr[i]);
+//     }
+// }
+// let result = result1.concat(result2.reverse());
+// console.log(result);
+
+// 6 misol
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < arr.length; i += 2) {
+//     console.log(arr[i]);
+// }
+
+// 7 misol
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let result = [];
+// for (let i = arr.length - 2; i >= 0; i -= 2) {
+//     result.push(arr[i]);
+// }
+// console.log(result.reverse());
